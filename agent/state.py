2f0071edfd315ebfec7b07b8ed79423ef8a99a8e
HiuKwongLung <lucashiukwong@gmail.com>
2026-05-04 23:11:39 -0400

    add state

---
+++ agent/state.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+from langgraph.graph.message import add_messages
+from langchain.messages import BaseMessage
+from dataclasses import dataclass, field
+
+@dataclass
+class AgentState:
+    # Research topic provided by the user
+    topic: str = ""
+    
+    # String output from web search tool
+    search_results: str = ""
+    
+    # String output from RAG tool
+    rag_results: str = ""
+    
+    # Markdown report from report writter tool
+    report: str = ""
+    
+    # Full conversation history
+    messages: Annotated[list[BaseMessage], add_messages] = field(default_factory=list)
